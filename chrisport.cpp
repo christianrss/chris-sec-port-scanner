cccac35599162bd543651edf6a770cf6b3bfb4a7
Christian Rafael <christianparadix@gmail.com>
2025-12-09 19:49:22 -0300

    Basic single port scan

--- chrisport.cpp
+++ chrisport.cpp
@@ -1,18 +1,52 @@
 #include <iostream>
 #include <WinSock2.h>
 #pragma comment(lib, "ws2_32.lib");
+using namespace std;
 
 void initWinsock() {
     WSADATA wsaData;
 
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        std::cerr << "Failed to initialized Winsock";
+        cerr << "Failed to initialized Winsock";
         exit(1);
     }
-    std::cout << "Winsock initiliazed successfully!" << std::endl;
+    cout << "Winsock initialiazed successfully!" << endl;
 
+}
+
+bool isOpen(const string &ip, int port) {
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock ==  INVALID_SOCKET) {
+        return false;
+    }
+
+    sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = inet_addr(ip.c_str());
+
+    int result = connect(sock, (struct sockaddr*)&address, sizeof(address));
+    closesocket(sock);
+
+    return result != SOCKET_ERROR;
 }
 
 int main() {
     initWinsock();
+
+    string targetIP;
+    int port;
+
+    cout << "Enter target IP: ";
+    cin >> targetIP;
+    cout << "Enter port: ";
+    cin >> port;
+
+    if (isOpen(targetIP, port)) {
+        cout << "Port " << port << " is open" << endl;
+    } else {
+        cout << "Port " << port << " is closed" << endl;
+    }
+
+    return 0;
 }
